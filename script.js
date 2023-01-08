c5ac1508f9f9487bf5f7a35f903eab1df5127852
Leonardo Santana <leonardo.santana@locaweb.com.br>
2023-01-08 17:07:20 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const numeros = document.querySelectorAll(input)
+const telaResultado = document.querySelector('#result')
+const buttonDarkMode = document.querySelector('.mode')
+let conta;
+
+function botao(value) {
+  conta = telaResultado.value += value
+}
+
+function limpar() {
+  telaResultado.value = ""
+}
+
+function calc() {
+  const resultado = eleval(conta)
+  telaResultado.value = resultado
+}
+
+function retirar() {
+  const palavraCompleta = telaResultado.value
+  const ultimaLetra = palavraCompleta.substring(0, telaResultado.value.length-1)
+  console.log(ultimaLetra)
+
+  telaResultado.value = ultimaLetra
+}
+
+buttonDarkMode.addEventListener('click', function() {
+  document.documentElement.classList.toggle('ativo')
+  buttonDarkMode.classList.toggle('ativo')
+})
